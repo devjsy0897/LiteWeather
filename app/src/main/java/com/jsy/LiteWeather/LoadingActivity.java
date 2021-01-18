73b207cc8758d5b2f0cc34f924a0063e703638c3
JeongSeongYeon <devjsy0897@gmail.com>
2021-01-18 15:01:15 +0900

    시작페이지 만들고 아이콘 넣음

---
+++ app/src/main/java/com/jsy/LiteWeather/LoadingActivity.java
@@ -0,0 +1,32 @@
+package com.jsy.LiteWeather;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+
+
+public class LoadingActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
